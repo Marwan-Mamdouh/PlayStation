0cc06612100076e93789e88e093fd67f0675bc97
Marwan-Mamdouh <marwanabdelmajjed@gmail.com>
2024-12-11 02:02:44 +0200

    reformat DatabaseConnection class with google style

--- src/main/java/App/DatabaseConnection.java
+++ src/main/java/App/DatabaseConnection.java
@@ -1,34 +1,36 @@
 package App;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Connection;
 
 public class DatabaseConnection {
-    private static DatabaseConnection instance;
-    private final Connection connection;
 
-    private DatabaseConnection() {
-        try {// Example for SQLITE /src/main/resources/database
-            String url = "jdbc:sqlite:/home/marwanmamdouh/java labs/Playstation/src/main/resources/database/Play.db";
-            connection = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            throw new RuntimeException("Error connecting to the database", e);
-        }
+  private static DatabaseConnection instance;
+  private final Connection connection;
+
+  private DatabaseConnection() {
+    try {// Example for SQLITE /src/main/resources/database
+      String url = "jdbc:sqlite:/home/marwanmamdouh/java labs/Playstation/src/main/"
+          + "resources/database/Play.db";
+      connection = DriverManager.getConnection(url);
+    } catch (SQLException e) {
+      throw new RuntimeException("Error connecting to the database", e);
     }
+  }
 
-    public static DatabaseConnection getInstance() {
+  public static DatabaseConnection getInstance() {
+    if (instance == null) {
+      synchronized (DatabaseConnection.class) {
         if (instance == null) {
-            synchronized (DatabaseConnection.class) {
-                if (instance == null) {
-                    instance = new DatabaseConnection();
-                }
-            }
+          instance = new DatabaseConnection();
         }
-        return instance;
+      }
     }
+    return instance;
+  }
 
-    public Connection getConnection() {
-        return connection;
-    }
+  public Connection getConnection() {
+    return connection;
+  }
 }
